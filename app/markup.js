52885f88c6d6e439caa1d6df0328edecca3e1c52
Ryan Tsao <rtsao@uber.com>
2016-11-28 18:02:57 -0800

    Update Google Analytics script

--- app/markup.js
+++ app/markup.js
@@ -25,19 +25,13 @@ export default ({children, links, typekitKey, title, gaKey, appContent, styletro
   </body>
   <script src="/client.bundle.js"></script>
   {gaKey ? <script dangerouslySetInnerHTML={{__html: gaScript(gaKey)}}/> : ''}
+  {gaKey ? <script src="https://www.google-analytics.com/analytics.js" async defer/> : ''}
   </html>
 )};
 
 function gaScript(key) {
-  return (
-    `
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', '${key}', 'auto');
-    ga('send', 'pageview');
-
-    `);
+  return [
+    `window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;`,
+    `ga('create','${key}','auto');ga('send','pageview')`
+  ].join('');
 }
